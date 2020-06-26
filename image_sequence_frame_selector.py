9cec59c5039c35867eb6584026d8a02bbef70178
apdupuis <Alexander.P.Dupuis@gmail.com>
2020-06-25 21:14:05 -0400

    The image sequence reader now uses a config file to load and save the last source and destination directories

--- image_sequence_frame_selector.py
+++ image_sequence_frame_selector.py
@@ -10,6 +10,7 @@ import os
 import sys
 import imghdr # for checking if file is a valid image
 from shutil import copyfile
+import json # for config file 
 
 import Tkinter, tkFileDialog
 
@@ -17,7 +18,7 @@ import Tkinter, tkFileDialog
 root = Tkinter.Tk()
 root.withdraw()
 
-src_directory = tkFileDialog.askdirectory()
+
 
 display_scalar = 0.75 #how large is the image we see when editing 
 current_image_index = 0
@@ -41,6 +42,35 @@ def update_frame(frame_offset):
 	load_new_image = True
 	print("Current frame: "+str(current_image_index))
 	return
+
+# check if we have a config file - if not, make one
+# the config file will contain the last directories used for loading and saving 
+
+if os.path.exists("image_sequence_frame_selector_config.json"):
+	with open("image_sequence_frame_selector_config.json") as config_file:
+		config_file_data = json.load(config_file)
+		previous_src_directory = config_file_data["previous_src_directory"]
+		previous_dst_directory = config_file_data["previous_dst_directory"]
+		config_file.close()
+else:
+	with open("image_sequence_frame_selector_config.json", "w+") as config_file:
+		config_file_data = {}
+		previous_src_directory = ""
+		previous_dst_directory = ""
+		config_file_data["previous_src_directory"] = previous_src_directory
+		config_file_data["previous_dst_directory"] = previous_dst_directory
+		json.dump(config_file_data, config_file)
+		config_file.close()
+
+
+src_directory = tkFileDialog.askdirectory(initialdir=previous_src_directory)
+
+# save source directory in config file 
+with open("image_sequence_frame_selector_config.json", "w") as config_file:
+	previous_src_directory = src_directory
+	config_file_data["previous_src_directory"] = previous_src_directory
+	json.dump(config_file_data, config_file)
+	config_file.close()
 
 # make a list of files in our directories 
 file_list = []
@@ -86,11 +116,17 @@ while(1):
 		update_frame(100)
 	elif k==115: # if 's', save the frame
 		if output_directory == "":
-			output_directory = tkFileDialog.askdirectory()
+			output_directory = tkFileDialog.askdirectory(initialdir=previous_dst_directory)
 		if output_directory != "":
 			output_file = os.path.join(output_directory, file_name)
 			print("Copying to "+output_file)
 			copyfile(file_path, output_file)
+			# save destination directory in config file 
+			with open("image_sequence_frame_selector_config.json", "w") as config_file:
+				previous_dst_directory = output_directory
+				config_file_data["previous_dst_directory"] = previous_dst_directory
+				json.dump(config_file_data, config_file)
+				config_file.close()
 	else:
 		print(k)
 
